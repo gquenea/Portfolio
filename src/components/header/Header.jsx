75d713a174005351e7f7b22e78d1e5d3093cd974
unknown <quenea.gislain@gmail.com>
2024-12-19 10:46:38 +0100

    Initial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import "./Header.css";
+
+export default function Header() {
+  const handleClick = () => {
+    window.location.href = "/";
+  };
+
+  return (
+    <div className="header">
+      <div onClick={handleClick}>
+        QueneaGislain<span className="point">.</span>
+        <span className="underscore">_</span>
+      </div>
+      <nav>
+        <ul>
+          <li>{"// Accueil"}</li>
+          <li>{"// Compétences"}</li>
+          <li>{"// Projets"}</li>
+          <li>{"// Expériences"}</li>
+          <li>{"// Contact"}</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
